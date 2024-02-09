f560c5f20918d25f7923d080dc68dca644e9bdb5
Viktor <130213112+ViktorOleshek@users.noreply.github.com>
2024-02-09 16:28:15 +0200

    Add files via upload

---
+++ ACD_lab11/BoyerMooreAlgorithm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ACD_lab11
+{
+  internal class BoyerMooreAlgorithm
+  {
+    private int [] BuildBadCharShift(string pattern)
+    {
+      int m = pattern.Length;
+      int [] badCharShift = new int [256];
+
+      for (int i = 0; i < badCharShift.Length; i++)
+        badCharShift [i] = m; // якщо символ не входить у паттерн, то зсув = довжині паттерну
+
+      for (int i = 0; i < m - 1; i++)
+        badCharShift [pattern [i]] = m - 1 - i;
+
+      return badCharShift;
+    }
+
+    public int Search(string text, string pattern, TextBox textBox_Log)
+    {
+      int n = text.Length;
+      int m = pattern.Length;
+
+      int [] badCharShift = BuildBadCharShift(pattern);
+
+
+      int i = m - 1; // початкове положення паттерну у тексті
+
+      textBox_Log.Text += "Stop symbols: ";
+      while (i < n)
+      {
+        int j = m - 1;
+
+        while (j >= 0 && (text [i] == pattern [j] || pattern [j] == ' '))
+        {
+          i--;
+          j--;
+        }
+
+        if (j < 0)
+        {
+
+          textBox_Log.Text += "\n";
+          return i + 1; // знайдено входження
+        }
+
+        int badCharShiftValue = badCharShift [text [i]];
+        textBox_Log.Text += text [i] + "/";
+        int spaceShift = pattern.Substring(j + 1).LastIndexOf(' ');
+
+        i += Math.Max(badCharShiftValue, spaceShift + 1);
+      }
+      textBox_Log.Text += "\n";
+
+      return -1; // входжень не знайдено
+    }
+  }
+}
---
+++ ACD_lab11/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ACD_lab11
+{
+  /// <summary>
+  /// Логика взаимодействия для MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+    private bool ValidateTextBox(TextBox textBox)
+    {
+      if (string.IsNullOrWhiteSpace(textBox.Text))
+      {
+        MessageBox.Show("Будь ласка, введіть дані в TextBox!");
+        return false;
+      }
+      else
+      {
+        string enteredData = textBox.Text;
+        return true;
+      }
+    }
+    void FindWordWithLeastVowels(out string wordWithLeastVowels)
+    {
+      string inputText = textBox_Text1.Text;
+
+      string [] words = inputText.Split(' ');
+
+      wordWithLeastVowels = "";
+
+      int minVowelCount = int.MaxValue;
+
+      foreach (string word in words)
+      {
+        int vowelCount = CountVowels(word);
+
+        if (vowelCount < minVowelCount)
+        {
+          minVowelCount = vowelCount;
+          wordWithLeastVowels = word;
+        }
+      }
+    }
+
+    int CountVowels(string word)
+    {
+      int count = 0;
+      string vowels = "aeiouAEIOU";
+
+      foreach (char c in word)
+      {
+        if (vowels.Contains(c))
+        {
+          count++;
+        }
+      }
+
+      return count;
+    }
+    string ReverseWord(string inputWord)
+    {
+      char [] charArray = inputWord.ToCharArray();
+      Array.Reverse(charArray);
+      return new string(charArray);
+    }
+
+    /// <summary>
+    /// Обчислює префікс-функцію для заданого образу.
+    /// </summary>
+    /// <returns>Масив значень префікс-функції.</returns>
+    private void button_Running(object sender, RoutedEventArgs e)
+    {
+      if (ValidateTextBox(textBox_Text1) && ValidateTextBox(textBox_Text2))
+      {
+        button_ClearLog(sender, e);
+        FindWordWithLeastVowels(out string wordWithLeastVowels);
+        textBox_Log.Text += wordWithLeastVowels + '\n';
+        wordWithLeastVowels = ReverseWord(wordWithLeastVowels);
+        textBox_Log.Text += wordWithLeastVowels + '\n';
+
+        BoyerMooreAlgorithm boyerMooreAlgorithm = new BoyerMooreAlgorithm();
+        int index = boyerMooreAlgorithm.Search(textBox_Text2.Text, 
+          wordWithLeastVowels, textBox_Log);
+        if (index == -1)
+        {
+          textBox_Log.Text += "No matches found\n";
+        }
+        else
+        {
+          textBox_Log.Text += "Match found at index: " + index + "\n";
+        }
+      }
+      else
+      {
+        return;
+      }
+    }
+    private void button_Exit(object sender, RoutedEventArgs e)
+    {
+      Environment.Exit(0);
+    }
+    private void button_ClearLog(object sender, RoutedEventArgs e)
+    {
+      textBox_Log.Clear();
+    }
+  }
+}
